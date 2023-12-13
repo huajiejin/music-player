6979a658511ae3ea302a18d67fbc700abbf94cba
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2023-12-13 02:05:02 -0500

    fix bug

--- app/src/main/java/com/jinshub/musicplayer/fragments/MusicPlayerFragment.java
+++ app/src/main/java/com/jinshub/musicplayer/fragments/MusicPlayerFragment.java
@@ -3,6 +3,7 @@ package com.jinshub.musicplayer.fragments;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -23,28 +24,51 @@ public class MusicPlayerFragment extends Fragment {
     // Tag for logging messages
     private static final String LOG_TAG = "MusicPlayerFragment";
     private MusicViewModel musicViewModel;
-
-    private Music selectedSong = MusicUtil.getDefaultMusicList().get(0);
     private MediaPlayer mediaPlayer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.i(LOG_TAG, "Creating MusicPlayerFragment");
         binding = FragmentMusicPlayerBinding.inflate(inflater, container, false);
 
         musicViewModel = new ViewModelProvider(requireActivity()).get(MusicViewModel.class);
+
+
+        Music currentSong = null;
+        // Restore state
+        if (savedInstanceState != null) {
+            boolean isPlaying = savedInstanceState.getBoolean("isPlaying");
+            int selectedSongIndex = savedInstanceState.getInt("selectedSongIndex");
+            int currentPosition = savedInstanceState.getInt("currentPosition");
+            Log.i(LOG_TAG, "Restoring state: isPlaying="+ isPlaying +", currentPosition="+ currentPosition);
+            if (isPlaying && selectedSongIndex > -1) {
+                currentSong = musicViewModel.getSongs().get(selectedSongIndex);
+            }
+        }
+
+        if (currentSong != null) {
+            musicViewModel.currentSong = currentSong;
+        }
+
         musicViewModel.getSelectedSong().observe(getViewLifecycleOwner(), new Observer<Music>() {
             @Override
             public void onChanged(Music song) {
                 // play the selected song
+                Log.i(LOG_TAG, "Selected song changed to "+ song.getTitle());
                 playSong(song);
             }
         });
 
         // if a song is already selected, display it
-        if (selectedSong != null) {
-            displaySong(selectedSong);
+        if (musicViewModel.currentSong != null) {
+            displaySong(musicViewModel.currentSong);
             binding.playPauseButton.setImageResource(R.drawable.ic_play);
+        }
+
+        // if a song is playing, display the pause button
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            binding.playPauseButton.setImageResource(R.drawable.ic_pause);
         }
 
         // set up the buttons to play the previous song, next song, and play/pause the current song
@@ -68,7 +92,7 @@ public class MusicPlayerFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Log.i(LOG_TAG, "playButton clicked");
-                togglePlayingSong(selectedSong);
+                togglePlayingSong(musicViewModel.currentSong);
             }
         });
 
@@ -82,12 +106,14 @@ public class MusicPlayerFragment extends Fragment {
             Log.i(LOG_TAG, "song is null");
             return;
         }
-        if (song != selectedSong) {
-            selectedSong = song;
+        if (song != musicViewModel.currentSong) {
+            musicViewModel.currentSong = song;
             displaySong(song);
         }
-        if (mediaPlayer != null) {
-            mediaPlayer.release();
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            // stop the current song
+            mediaPlayer.stop();
+            binding.playPauseButton.setImageResource(R.drawable.ic_play);
         }
         int resourceId = song.getMusicResourceId();
         mediaPlayer = MediaPlayer.create(getActivity(), resourceId);
@@ -98,13 +124,16 @@ public class MusicPlayerFragment extends Fragment {
     
     // Pause the music if it is playing, or play the music if it is paused
     public void togglePlayingSong(Music song) {
+        if (song == null) {
+            return;
+        }
         Log.i(LOG_TAG, "togglePlayingSong("+ song.getTitle() +")");
         if (song == null) {
             Log.i(LOG_TAG, "song is null");
             return;
         }
-        if (song != selectedSong) {
-            selectedSong = song;
+        if (song != musicViewModel.currentSong) {
+            musicViewModel.currentSong = song;
             displaySong(song);
         }
         if (mediaPlayer == null) {
@@ -125,6 +154,18 @@ public class MusicPlayerFragment extends Fragment {
     public void displaySong(Music song) {
         binding.songTitle.setText(song.getTitle());
         binding.albumArt.setImageResource(song.getAlbumArtResourceId());
-//        binding.artistName.setText(song.getArtist());
+        // binding.artistName.setText(song.getArtist());
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mediaPlayer != null) {
+            outState.putBoolean("isPlaying", mediaPlayer.isPlaying());
+            int index = musicViewModel.getSongs().indexOf(musicViewModel.getSelectedSong().getValue());
+            outState.putInt("selectedSongIndex", index);
+            outState.putInt("currentPosition", mediaPlayer.getCurrentPosition());
+            Log.i(LOG_TAG, "Saving state: isPlaying="+ mediaPlayer.isPlaying() +", selectedSongIndex="+ musicViewModel.getSongs().indexOf(musicViewModel.currentSong) +", currentPosition="+ mediaPlayer.getCurrentPosition());
+        }
     }
 }
--- app/src/main/java/com/jinshub/musicplayer/viewmodels/MusicViewModel.java
+++ app/src/main/java/com/jinshub/musicplayer/viewmodels/MusicViewModel.java
@@ -1,5 +1,7 @@
 package com.jinshub.musicplayer.viewmodels;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -13,13 +15,20 @@ import java.util.List;
 public class MusicViewModel extends ViewModel {
     private List<Music> songs = MusicUtil.getDefaultMusicList();
     private final MutableLiveData<Music> selectedSong = new MutableLiveData<>();
+    public Music currentSong = MusicUtil.getDefaultMusicList().get(0);
+    // Tag for logging messages
+    private static final String LOG_TAG = "MusicViewModel";
 
     public List<Music> getSongs() {
         return songs;
     }
 
     public void selectSong(Music song) {
+        if (song == currentSong) {
+            return;
+        }
         selectedSong.setValue(song);
+        currentSong = song;
     }
 
     public LiveData<Music> getSelectedSong() {
@@ -28,7 +37,7 @@ public class MusicViewModel extends ViewModel {
 
     // select the previous song in the playlist, or the last song if the current song is the first song
     public void selectPrevSong() {
-        int index = songs.indexOf(selectedSong.getValue());
+        int index = getIndexOfSong(currentSong);
         if (index > 0) {
             selectedSong.setValue(songs.get(index - 1));
         } else {
@@ -38,11 +47,25 @@ public class MusicViewModel extends ViewModel {
     
     // select the next song in the playlist, or the first song if the current song is the last song
     public void selectNextSong() {
-        int index = songs.indexOf(selectedSong.getValue());
+        int index = getIndexOfSong(currentSong);
+        Log.i(LOG_TAG, "selectNextSong " + index);
         if (index < songs.size() - 1) {
             selectedSong.setValue(songs.get(index + 1));
         } else {
             selectedSong.setValue(songs.get(0));
         }
+    }
+
+    public Integer getIndexOfSong(Music song) {
+        if (song == null) {
+            return -1;
+        }
+        // compare their titles
+        for (int i = 0; i < songs.size(); i++) {
+            if (songs.get(i).getTitle().equals(song.getTitle())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
